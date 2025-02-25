dd0e9263a8e78114619d01c74f16f613d159e31c
Chris <sevenwonders.info@gmail.com>
2025-02-25 12:28:29 +0200

    pre-processing phase code to tokenize and lower-case words in records

--- .venv/nlp_scripts/pre-processing.py
+++ .venv/nlp_scripts/pre-processing.py
@@ -1,5 +1,21 @@
 import numpy as np
 from nltk.tokenize import word_tokenize
 from collections import defaultdict
+import pandas as pd
+import nltk as nltk
 
+corpus = pd.read_csv('C:/Users\chris\PycharmProjects\PythonProject\.venv\data\Insurance_Company_Complaints__Resolutions__Status__and_Recoveries.csv')
+complaints = []
+vocabulary = []
 
+for record in corpus:
+    r_tokenized = word_tokenize(record)
+    complaint = [word.lower() for word in r_tokenized if word.isalpha()]
+    complaints.append(complaint)
+    for word in complaint:
+        if word not in vocabulary:
+            vocabulary.append(word)
+
+len_vocabulary = len(vocabulary)
+print(len_vocabulary)
+print(vocabulary)
