c88eedd1c124356d1754b8a987095a7560d2bc67
Chris <sevenwonders.info@gmail.com>
2025-02-25 15:28:06 +0200

    Fixes #4 `stopwords` module is not directly iterable

--- .venv/nlp_scripts/pre-processing.py
+++ .venv/nlp_scripts/pre-processing.py
@@ -5,6 +5,8 @@ import pandas as pd
 import nltk as nltk
 nltk.download('punkt')
 nltk.download('punkt_tab')
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 
 corpus = pd.read_csv('C:/Users\chris\PycharmProjects\PythonProject\.venv\data\Insurance_Company_Complaints__Resolutions__Status__and_Recoveries.csv')
 complaints = []
@@ -17,7 +19,16 @@ for record in corpus.itertuples(index=False, name=None):
     complaints.append(complaint)
     for word in complaint:
         if word not in vocabulary:
-            vocabulary.append(word)
+            vocabulary.append(word) #1019 records in total
+
+    stop_words = set(stopwords.words('english'))
+    for v in vocabulary:
+        if v in stop_words:
+            vocabulary.remove(v) #reduced to 996 records
+
+    for v in vocabulary:
+        if len(v) < 2:
+            vocabulary.remove(v) #reduced to 989 records
 
 len_vocabulary = len(vocabulary)
 print(len_vocabulary)
