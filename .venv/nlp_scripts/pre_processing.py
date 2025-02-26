787eb92653595ad131af224a468b44a36842891b
Chris <sevenwonders.info@gmail.com>
2025-02-26 17:41:06 +0200

    complaints and vocabulary data structures were merged, checks were united and optimized

--- .venv/nlp_scripts/pre_processing.py
+++ .venv/nlp_scripts/pre_processing.py
@@ -10,26 +10,11 @@ nltk.download('stopwords')
 
 corpus = pd.read_csv('C:/Users\chris\PycharmProjects\PythonProject\.venv\data\Insurance_Company_Complaints__Resolutions__Status__and_Recoveries.csv')
 complaints = []
-vocabulary = []
+
+stop_words = set(stopwords.words('english'))
 
 for record in corpus.itertuples(index=False, name=None):
     record_text = " ".join([str(value) for value in record])
     r_tokenized = word_tokenize(record_text)
-    complaint = [word.lower() for word in r_tokenized if word.isalpha()]
-    complaints.append(complaint)
-    for word in complaint:
-        if word not in vocabulary:
-            vocabulary.append(word) #1019 records in total
-
-    stop_words = set(stopwords.words('english'))
-    for v in vocabulary:
-        if v in stop_words:
-            vocabulary.remove(v) #reduced to 996 records
-
-    for v in vocabulary:
-        if len(v) < 2:
-            vocabulary.remove(v) #reduced to 989 records
-
-len_vocabulary = len(vocabulary)
-print(len_vocabulary)
-print("\n".join(vocabulary))
+    complaint = [word.lower() for word in r_tokenized if word.isalpha() and word not in stop_words and len(word) >= 2]
+    complaints.append(" ".join(complaint))
